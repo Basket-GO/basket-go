566ff46e7d4566bfccdf14105a3a64da4d352b2e
Louis Gallet <me@louisgallet.fr>
2023-01-27 12:02:59 +0100

    feat: :construction:  Starting POO
    Refactor folder and starting the creation of POO for buttons

---
+++ component/button.py
@@ -0,0 +1,25 @@
+import pygame
+import component as Component
+# button class
+
+
+class Button(Component):
+    def __init__(self, x, y, image, hoover):
+        super().__init__(x, y)
+        self.__image = image
+        self.__hoover = hoover
+        self.__clicked = False
+
+    def draw(self, surface):
+        # get mouse position
+        pos = pygame.mouse.get_pos()
+        image = self.__image
+        # check mouseover and clicked conditions
+        if image.get_rect().collidepoint(pos):
+            image = self.__hoover
+            if pygame.mouse.get_pressed()[0] == 1 and self.__clicked == False:
+                self.__clicked = True
+        elif pygame.mouse.get_pressed()[0] == 0:
+            self.__clicked = False
+        # draw button on screen
+        surface.blit(image, (self.rect.x, self.rect.y))
---
+++ component/component.py
@@ -0,0 +1,19 @@
+class Component:
+    def __init__(self, x: int = 0, y: int = 0) -> None:
+        self.__x = x
+        self.__y = y
+
+    def get_x(self) -> int:
+        return self.__x
+
+    def get_y(self) -> int:
+        return self.__y
+
+    def set_x(self, x: int) -> None:
+        self.__x = x
+
+    def set_y(self, y: int) -> None:
+        self.__y = y
+
+    def draw(self, surface):
+        pass
